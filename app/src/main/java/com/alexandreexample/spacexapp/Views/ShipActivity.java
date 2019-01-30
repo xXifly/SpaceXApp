adecfc681383e15a16cc61ff7760322f9ea5c7ab
Alexandre Joron <alexandre.joron@protonmail.com>
2019-01-30 19:14:55 +0100

    designed

--- app/src/main/java/com/alexandreexample/spacexapp/Views/ShipActivity.java
+++ app/src/main/java/com/alexandreexample/spacexapp/Views/ShipActivity.java
@@ -1,58 +1,70 @@
 package com.alexandreexample.spacexapp.Views;
 
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.alexandreexample.spacexapp.Models.Ship;
 import com.alexandreexample.spacexapp.R;
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.RequestManager;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
 
-public class ShipActivity extends AppCompatActivity {
+public class ShipActivity extends AppCompatActivity implements View.OnClickListener {
 
     Ship ship;
     ImageView mImageView;
+    private RequestManager glide;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.ship_activity);
 
-        TextView mTextView = findViewById(R.id.textView);
+        TextView tvPort = findViewById(R.id.port);
+        TextView tvStatus = findViewById(R.id.status);
+        Button btnDetails = findViewById(R.id.details);
         mImageView = findViewById(R.id.imageView);
+
+        btnDetails.setOnClickListener(this);
 
         Intent intent = getIntent();
         ship = (Ship) intent.getSerializableExtra("data");
 
         setTitle(ship.getShipName());
 
-        //setPicture();
+        setPicture();
 
+        if(ship.getHomePort() != null){
+            tvPort.setText(String.format("Port : %s", ship.getHomePort()));
+        }
+        if(ship.getStatus() != null) {
+            tvStatus.setText(String.format("Status : %s", ship.getStatus()));
+        }
+        if(ship.getUrl() == null) {
+            btnDetails.setVisibility(View.INVISIBLE);
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent;
+        switch (v.getId()) {
+            case R.id.details:
+                intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse(ship.getUrl()));
+                startActivity(intent);
+                break;
+        }
     }
 
     public void setPicture() {
-        try {
-            URL url = new URL(ship.getImage());
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            connection.setDoInput(true);
-            //connection.connect();
-            InputStream input = connection.getInputStream();
-            Bitmap bitmap = BitmapFactory.decodeStream(input);
-            mImageView.setImageBitmap(bitmap);
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        Glide.with(this).load(ship.getImage()).into(mImageView);
     }
 
 }
